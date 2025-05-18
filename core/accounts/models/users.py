ffd8c0bc38b24728cf451a8ad0cbd2a328b57d35
mahdi921 <19709896+mahdi921@users.noreply.github.com>
2025-05-19 01:27:40 +0330

    Add user and profile models with custom user manager; update URL patterns for Djoser integration

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -49,17 +49,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-
-
-class Profile(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
-    # username
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    bio = models.TextField()
-    avatar = models.ImageField(blank=True, null=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.email
