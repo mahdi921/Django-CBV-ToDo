c7a1c4fac89e6a97073531fe7c70686e5978845f
Mahdi Afshar <mahdi921@users.noreply.github.com>
2025-10-20 15:32:31 +0330

    Add management command to generate dummy tasks for testing

---
+++ core/todo/management/commands/task_gen.py
@@ -0,0 +1,31 @@
+from faker import Faker
+from django.core.management.base import BaseCommand
+from todo.models import Task
+from accounts.models import User, Profile
+import random
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    help = "Generating dummy tasks for test"
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **kwargs):
+        user = User.objects.create_user(
+            email=self.fake.email(),
+            password="Fakepass@123456"
+        )
+        profile = Profile.objects.get(user=user)
+        profile.first_name = self.fake.first_name()
+        profile.last_name = self.fake.last_name()
+        profile.bio = self.fake.text(max_nb_chars=1000)
+        profile.save()
+        for _ in range(5):
+            Task.objects.create(
+                author=profile,
+                title=self.fake.text(max_nb_chars=255),
+                completed=random.choice([True, False]),
+            )
